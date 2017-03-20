416506b428673c67b377184ed8f689e38246e7c7
ddimmxxgmail.com <dimmxx@gmail.com>
2017-03-20 21:55:08 +0200

    itcenterOOP2

---
+++ src/collections/linkedList/ArrayListMine.java
@@ -0,0 +1,78 @@
+package collections.linkedList;
+
+/**
+ * Created by ddimmxxgmail.com on 3/20/17.
+ */
+public class ArrayListMine <E> implements List <E> {
+
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public void addAll(List<? extends E> collection) {
+
+    }
+
+    @Override
+    public void addAll(int index, List<? extends E> collection) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int indexFrom, int indexTo) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
